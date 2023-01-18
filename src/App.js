6cd85f12c0cf783ee3bd95db656d8d94258d7417
henry <henry194557@gmail.com>
2023-01-18 23:32:50 +0800

    add repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
+import Home from './Home';
+import Lottery from './pages/Lottery/Lottery';
+import Sum from './pages/Sum/Sum';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/Sum' element={<Sum/> }></Route>
+        <Route path='/Lottery' element={<Lottery/>}></Route>
+        <Route></Route>
+      </Routes>
+
+      <Link to='/Lottery'>Lottery</Link>
+      <Link to='/Sum'>Sum</Link>
+      <Link></Link>
+
+    </BrowserRouter>
   );
 }
 
